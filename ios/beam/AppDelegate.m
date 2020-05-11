62ac8fa163305ff5ccf46c9ebeffd1136a0c35af
Mohamed Javid <smohamedjavid@gmail.com>
2020-05-12 01:36:35 +0800

    Update AppDelegate.m

--- ios/beam/AppDelegate.m
+++ ios/beam/AppDelegate.m
@@ -28,7 +28,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-   [GMSServices provideAPIKey:@"AIzaSyCmQQkTsQjLZ0nkk0VvTSwvb3_8rilbyVE"];
+   [GMSServices provideAPIKey:@"YOUR_GOOGLE_MAPS_API_KEY"];
 #if DEBUG
   InitializeFlipper(application);
 #endif
